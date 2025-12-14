c8a4821abc224986986231fad569f7ee748d4e27
fikriari00 <42141393+fikriari00@users.noreply.github.com>
2025-12-14 09:56:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -119,3 +119,8 @@ def get_signal():
 @app.on_event("startup")
 async def startup():
     asyncio.create_task(client.start())
+    
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
